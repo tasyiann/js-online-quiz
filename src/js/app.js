8dcae5a618edbd642b76fa802c4ec7af90c4490b
Anastasios <ay222cn@lnu.se>
2017-10-01 12:24:54 +0200

    trying...

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+var pageCounter = 1
+var questionContainer = document.getElementById('question')
+var start_btn = document.getElementById('start_btn')
+// Click start button to start the game
+start_btn.addEventListener('click', function () {
+  var Request = new XMLHttpRequest(true)
+
+})
